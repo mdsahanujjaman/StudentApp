8efbf697c517d053b4f8aa2886a8a6f0f8d9895f
Mdsah <Mdsahanujjaman17052002@gmail.com>
2025-12-09 10:55:02 +0530

    Add grade calculation logic

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Grade Calculator") 
+marks = int(input("Enter marks (0–100): "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Your Grade:", grade)
